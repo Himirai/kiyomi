28257701185d4aeb070d17ad975a5d21c0ea13be
zhogoshi <hogoshi@yandex.ru>
2024-05-24 14:40:14 +0700

    nothing was there. just against 1.0.0 version

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
 
 val mainClassName = "Kiyomi"
 group = "dev.himirai.${mainClassName.lowercase()}"
-version = "1.1.4"
+version = "1.0.0"
 val internal = "$group.internal"
 
 repositories {
